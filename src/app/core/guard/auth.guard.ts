f4eae9adce882901a171b14a7f3caa38c6b916fd
areangonzalez <areangonzalez@gmail.com>
2022-02-01 11:53:40 -0300

    TG-101 #Closed se crea la configuracion de seguridad para el ingreso de un usuario

---
+++ src/app/core/guard/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AutenticacionService } from '../service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor( private _router: Router, private _auth: AutenticacionService ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+      let autorizacion = this._auth.loggedIn;
+      const roles = route.data['rol'];
+      let cont = 0;
+      if (roles && autorizacion) {
+          for (const rol of roles) {
+            if ( rol === autorizacion.rol) {
+              cont++;
+            }
+          }
+          return autorizacion && cont > 0;
+      }
+      if (autorizacion) { return true; }
+      // not logged in so redirect to login page with the return url
+
+      this._router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    return this.canActivate(route, state);
+  }
+
+}
